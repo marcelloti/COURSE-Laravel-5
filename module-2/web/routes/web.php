26ed276eb8418c76a8859218c411f7f7c4bc49cb
Marcello Costa <marcello88costa@yahoo.com.br>
2021-02-28 14:39:14 -0300

    Making module 2

--- module-2/web/routes/web.php
+++ module-2/web/routes/web.php
@@ -14,3 +14,16 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function(){
+    Route::resource('clients', 'ClientsController');
+});
+
+Route::name('meu-nome')
+    ->get('/rota-nomeada/qualquer-coisa', function () {
+        echo "Hello World!!";
+    });
+
+Route::get('/rota-nomeada1/qualquer-coisa', function () {
+        echo "Hello World1!!";
+    })->name('meu-nome1');
